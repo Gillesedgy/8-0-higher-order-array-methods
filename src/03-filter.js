6945be1af04fc3c7c098620547894525bb6b149a
Gillesedgy <edgygilles@pursuit.org>
2022-07-27 17:04:13 -0400

    filter(prob2)

--- src/03-filter.js
+++ src/03-filter.js
@@ -13,14 +13,28 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsBySaib(songs) {}
+function getSongsBySaib(songs) {
+  const getSaib = songs.filter(song => {
+    if( song.artist === "Saib"){
+      return song.title;
+    }
+  })
+  return getSaib;
+}
 
 /**
  * Returns an array of all songs where the runtime is over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsOverThreeMinutes(songs) {}
+function getSongsOverThreeMinutes(songs) {
+  const threeOrMore = songs.filter(song => {
+    if(song.runtimeInSeconds >= 180){
+      return song.title;
+    }
+  })
+  return threeOrMore;
+}
 
 /**
  * Returns an array of songs where the song title is the same as the song album.
